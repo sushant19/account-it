31c562578fa978e238b89acd78920913fafcdc2d
ZIJ2005 <zij2005@gmail.com>
2011-05-17 15:00:49 +0000

    Настройка подключения к базе перенесена в конструктор Banking.EFData.EFStorage

---
+++ Banking/Banking.Domain/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Banking.Domain
+{
+    public class Session: EntityBase
+    {
+        public Guid SessionId { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
