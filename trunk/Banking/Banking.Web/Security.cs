404d0b7df93cb4eeb6615a1150fe09bbe1bb15e5
zij2005@gmail.com <zij2005@gmail.com@c9ee8a64-0b73-eec8-5376-bd8bcb1d1289>
2011-05-17 20:10:49 +0000

    * Добавлено шифрование пароля при передаче

--- trunk/Banking/Banking.Web/Security.cs
+++ trunk/Banking/Banking.Web/Security.cs
@@ -7,7 +7,7 @@ namespace Banking.Web
 {
     public static class Security
     {
-        public static string Key { get { return "111"; } }
+        public static string Key { get { return "f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae"; } }
         public static TimeSpan Timeout { get { return TimeSpan.FromMinutes(1); } }
     }
 }
