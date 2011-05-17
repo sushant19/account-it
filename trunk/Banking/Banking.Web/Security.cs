333e81a567e03f1d6174186b61b71728ba42a394
zij2005@gmail.com <zij2005@gmail.com@c9ee8a64-0b73-eec8-5376-bd8bcb1d1289>
2011-05-17 20:49:58 +0000

    * В классе Security хэш пароля теперь вычисляется в статическом конструкторе - пароль легко захардкодить.

--- trunk/Banking/Banking.Web/Security.cs
+++ trunk/Banking/Banking.Web/Security.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.Text;
+using System.Security.Cryptography;
+
 namespace Banking.Web
 {
     public static class Security
     {
-        public static string Key { get { return "f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae"; } }
+        static Security()
+        {
+            _key = HashOf("111");
+        }
+        private static string _key;
+        
+        public static string Key { get { return _key; } }
         public static TimeSpan Timeout { get { return TimeSpan.FromMinutes(1); } }
+
+        private static string HashOf(string str)
+        {
+            var encoding = new UTF8Encoding();
+            byte[] bytes = encoding.GetBytes(str);
+            
+            SHA256Managed sha = new SHA256Managed();
+            byte[] hash = sha.ComputeHash(bytes);
+
+            return BitConverter.ToString(hash).
+                Split('-').
+                    Aggregate("", (total, current) => total + current).
+                        ToLower();
+        }
     }
 }
