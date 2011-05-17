30c367be50f129860f9db3446eca6d064aa1a68d
ZIJ2005 <zij2005@gmail.com>
2011-05-17 21:54:18 +0000

    НАЖИВУЮ

--- Banking/Banking.Web/Scripts/Main.js
+++ Banking/Banking.Web/Scripts/Main.js
@@ -13,6 +13,18 @@ function assignBehavior(entityName) {
         return '/' + entityName + '/' + actionName + entityName;
     }
 
+    $('tr').live('dblclick', function () {
+        var url = makeUrl('Edit');
+        var target = $(this)
+        var query = parseId(target);
+        //var before = new Date();
+        $.post(url, query, function (data) {
+            //var after = new Date()
+            //alert(after.getTime() - before.getTime());
+            target.replaceWith(data);
+        }).error(function () { alert('fck...'); });
+    });
+
     $('.editButton').live('click', function () {
         var url = makeUrl('Edit');
         var target = $(this).parent().parent();
@@ -28,6 +40,25 @@ function assignBehavior(entityName) {
     $('.deleteButton').live('click', function () {
         var sure = confirm('Are you sure?');
         if (sure == true) {
+            var ids = $.map($('tr'), function (row) {
+                var checkbox = $(row).find('.itemCheck');
+                if (checkbox.prop('checked') == true) {
+                    return $(row).attr('data-id');
+                }
+                else {
+                    return null;
+                }
+            });
+            var url = makeUrl('Delete');
+            for (var i = 0; i < ids.length; i++) {
+                
+            }
+        }
+    });
+
+    $('.deleteButtonXXXX').live('click', function () {
+        var sure = confirm('Are you sure?');
+        if (sure == true) {
             var url = makeUrl('Delete');
             var target = $(this).parent().parent();
             var query = parseId(target);
@@ -39,6 +70,8 @@ function assignBehavior(entityName) {
             }).error(function () { alert('fck...'); })
         }
     });
+
+
 
     $('.saveButton').live('click', function () {
         var url = makeUrl('Save');
