180556dc98e1549ab3d5594d34a50f67bb398c40
TNK2005 <TNK2005@gmail.com>
2011-07-08 17:59:49 +0000

    added showModal, totally reworked

--- Banking/Banking.Web/Scripts/Main.js
+++ Banking/Banking.Web/Scripts/Main.js
@@ -2,6 +2,27 @@
     $('#freeow').freeow("Error", message, { classes: ["smokey", "error"] });
 }
 
+function showModal(data) {
+    $.modal(data, {
+        onOpen: function (dialog) {
+            dialog.overlay.fadeIn('normal', function () {
+                dialog.container.slideDown('normal', function () {
+                    dialog.data.fadeIn('normal');
+                });
+            });
+        },
+        onClose: function (dialog) {
+            dialog.data.fadeOut('normal', function () {
+                dialog.container.slideUp('normal', function () {
+                    dialog.overlay.fadeOut('normal', function () {
+                        $.modal.close(); // must call this!
+                    });
+                });
+            });
+        }
+    });
+}
+
 function assignBehavior(entityName) {
 
     function parseId(target) {
@@ -11,6 +32,7 @@ function assignBehavior(entityName) {
 
     // if session expired, then EnterCode will be returned
     // instead of whatever requested, so we need redirect...
+    //TODO: right solution to detect if data or document was returned
     function filterResponse(data) {
         if (data.indexOf("DOCTYPE") == -1) {
             return data;
@@ -34,10 +56,11 @@ function assignBehavior(entityName) {
         $.post(url, query, function (data) {
             //var after = new Date()
             //alert(after.getTime() - before.getTime());
-            target.replaceWith(filterResponse(data));
+            //target.replaceWith(filterResponse(data));
+            var data_filtered = filterResponse(data);
+            showModal(data_filtered);
         }).error(function () { showError('Edit request failed'); });
     });
-
 //    $('.editButton').live('click', function () {
 //        var url = makeUrl('Edit');
 //        var target = $(this).parent().parent();
@@ -51,49 +74,66 @@ function assignBehavior(entityName) {
 //    });
 
     $('.deleteButton').live('click', function () {
-        var sure = confirm('Are you sure?');
-        if (sure == true) {
-            var ids = $.map($('tr'), function (row) {
-                var checkbox = $(row).find('.itemCheck');
-                if (checkbox.prop('checked') == true) {
-                    return $(row).attr('data-id');
-                }
-                else {
-                    return null;
-                }
-            });
+        var ids = $.map($('tr'), function (row) {
+            var checkbox = $(row).find('.itemCheck');
+            if (checkbox.prop('checked') == true) {
+                return $(row).attr('data-id');
+            }
+            else {
+                return null;
+            }
+        });
+        if (ids.length == 0) {
+            showError("Select something first!");
+        }
+        else if (confirm('Are you sure?')) {
             var url = makeUrl('Delete');
             for (var i = 0; i < ids.length; i++) {
                 var query = { id: ids[i] };
                 $.post(url, query, function (response) {
                     $('tr').filter('[data-id=\"' + response.id + '\"]').remove();
                 }).error(function () { showError('Delete request failed'); });
+                //TODO: Sessions?
             }
         }
     });
 
-    $('.saveButton').live('click', function () {
+    $('.saveButton').live('click', function (e) {
+        e.preventDefault();
         var url = makeUrl('Save');
-        var target = $(this).parent().parent();
+        var target = $(this).parent().parent().parent();
+        id = target.attr('data-id');
+        var views = $('[data-id=' + id + ']');
         var query = parseFields(target);
         //var before = new Date();
         $.post(url, query, function (data) {
             //var after = new Date()
             //alert(after.getTime() - before.getTime());
-            target.replaceWith(filterResponse(data));
+            //target.replaceWith(filterResponse(data));
+            $.modal.close();
+            var data_filtered = filterResponse(data);
+            if (views.length > 1) {
+                views.replaceWith(data_filtered);
+            }
+            else {
+                $("#operations").children('tbody').append(data_filtered);
+            }
         }).error(function () { showError('Save request failed'); })
     });
 
     $('.cancelButton').live('click', function () {
-        var url = makeUrl('View');
-        var target = $(this).parent().parent();
-        var query = parseId(target);
-        //var before = new Date();
-        $.post(url, query, function (data) {
-            //var after = new Date()
-            //alert(after.getTime() - before.getTime());
-            target.replaceWith(filterResponse(data));
-        }).error(function () { showError('Cancel request failed'); })
+//        var url = makeUrl('View');
+//        var target = $(this).parent().parent();
+//        var query = parseId(target);
+//        //var before = new Date();
+//        $.post(url, query, function (data) {
+//            //var after = new Date()
+//            //alert(after.getTime() - before.getTime());
+//            //target.replaceWith(filterResponse(data));
+//            $.modal.close();
+//        }).error(function () { showError('Cancel request failed'); })
+//request not needed — we only hide modal dialog
+        $.modal.close();
     });
 
     $('.createButton').live('click', function () {
@@ -102,7 +142,9 @@ function assignBehavior(entityName) {
         $.post(url, {}, function (data) {
             //var after = new Date()
             //alert(after.getTime() - before.getTime());
-            $('#headings').after(filterResponse(data));
+            //$('#headings').after(filterResponse(data)); this is old version with putting new item within existing ones table
+            var data_filtered = filterResponse(data);
+            showModal(data_filtered);
         }).error(function () { showError('Create request failed'); })
     });
 
