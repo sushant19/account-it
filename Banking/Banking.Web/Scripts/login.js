575413734257118b5fef791dfaaa0049dee9e332
TNK2005 <TNK2005@c9ee8a64-0b73-eec8-5376-bd8bcb1d1289>
2011-09-09 17:27:24 +0000

    Improved errors handling in login.js (credit to TNK2005)

--- Banking/Banking.Web/Scripts/login.js
+++ Banking/Banking.Web/Scripts/login.js
@@ -21,14 +21,24 @@
             var hash = Crypto.SHA256(code);
             // sending login info
             $.post('Home/EnterCode', { code: hash }, function (response) {
+                
+                //revert ui disabling
+                $("input").each(function () { $(this).removeAttr("disabled"); });
+                $("#codeInput").removeClass("loading");
+
                 if (!response.error) {
                     window.location = '/operations';
                 } else {
-                    $("input").each(function () { $(this).removeAttr("disabled"); });
-                    $("#codeInput").removeClass("loading");
                     ui.handleError(response.error);
                 }
-            }).error(function () { alert('For some strange(probably, magic?) reason, ajax reqest could not reach the server. Please, inform system administrator.'); })
+            }).error(function () {
+                ui.showError('For some strange(probably, magic?) reason, ' 
+                    + 'ajax reqest could not reach the server. Please, inform system administrator.');
+                
+                //revert ui disabling
+                $("input").each(function () { $(this).removeAttr("disabled"); });
+                $("#codeInput").removeClass("loading");
+            })         
         });
     });
 })();
