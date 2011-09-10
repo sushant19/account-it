d6a02c9e942d03bcecf0cc5a1de4583fdbc3ecac
zij2005@gmail.com <zij2005@gmail.com@c9ee8a64-0b73-eec8-5376-bd8bcb1d1289>
2011-09-10 20:42:06 +0000

    * Some more refactoring in login.js

--- trunk/Banking/Banking.Web/Scripts/login.js
+++ trunk/Banking/Banking.Web/Scripts/login.js
@@ -1,49 +1,49 @@
- (function () {
-    $(document).ready(function () {
-        // removing menu
-        $("menu").children().each(function () {
-            $(this).remove();
-        });
-        // putting message in place of menu
-        $("menu").append("<h3>Welcome to the point of entrance</h3>");
-        // focusing on password field
-        $("#codeInput").focus();
-        // handling pass code submition
-        $("#enter_code_form").submit(function (e) {
-            e.preventDefault();
-            disableUI();
-            // making data
-            var code = $('#codeInput').prop('value');
-            var hash = Crypto.SHA256(code);
-            // sending login info
-            $.post('Home/EnterCode', { code: hash }, function (response) {
-                enableUI();
-                // handling response
-                if (!response.error) {
-                    // redirect to home
-                    window.location = '/';
-                } else {
-                    ui.handleError(response.error);
-                }
-            }).error(function () {
-                ui.showError('For some strange(probably, magic?) reason, '
+﻿ (function () {
+     $(document).ready(function () {
+         // removing menu
+         $("menu").children().each(function () {
+             $(this).remove();
+         });
+         // putting message in place of menu
+         $("menu").append("<h3>Welcome to the point of entrance</h3>");
+         // focusing on password field
+         $("#codeInput").focus();
+         // handling pass code submition
+         $("#enter_code_form").submit(function (e) {
+             e.preventDefault();
+             disableUI();
+             // making data
+             var code = $('#codeInput').prop('value');
+             var hash = Crypto.SHA256(code);
+             // sending login info
+             $.post('Home/EnterCode', { code: hash }, function (response) {
+                 // handling response
+                 if (!response.error) {
+                     // redirect to home
+                     window.location = '/';
+                 } else {
+                     ui.handleError(response.error);
+                 }
+             }).error(function () {
+                 ui.showError('For some strange(probably, magic?) reason, '
                     + 'ajax reqest could not reach the server. Please, inform system administrator.');
-                enableUI();
-            })
-        });
-    });
+             }).complete(function () {
+                 enableUI();
+             });
+         });
+     });
 
-    function disableUI() {
-        $("input").each(function () {
-            $(this).attr("disabled", "disabled");
-            $("#codeInput").addClass("loading");
-        });
-    }
+     function disableUI() {
+         $("input").each(function () {
+             $(this).attr("disabled", "disabled");
+             $("#codeInput").addClass("loading");
+         });
+     }
 
-    function enableUI() {
-        $("input").each(function () { $(this).removeAttr("disabled"); });
-        $("#codeInput").removeClass("loading");
-        $('#codeInput').val('');
-        $('#codeInput').focus();
-    }
-})();
+     function enableUI() {
+         $("input").each(function () { $(this).removeAttr("disabled"); });
+         $("#codeInput").removeClass("loading");
+         $('#codeInput').val('');
+         $('#codeInput').focus();
+     }
+ })();
