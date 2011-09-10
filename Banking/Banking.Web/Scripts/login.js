9f9b008ed5afd2ac21d86e0611edf16baf2e5c91
ZIJ2005 <zij2005@gmail.com>
2011-09-10 19:49:43 +0000

    * login.js bug fixes (credit to TNK2005) * login.js refactoring

--- Banking/Banking.Web/Scripts/login.js
+++ Banking/Banking.Web/Scripts/login.js
@@ -1,44 +1,48 @@
 ﻿(function () {
     $(document).ready(function () {
-        //modifying menu
+        // removing menu
         $("menu").children().each(function () {
             $(this).remove();
         });
+        // putting message in place of menu
         $("menu").append("<h3>Welcome to the point of entrance</h3>");
-
+        // focusing on password field
         $("#codeInput").focus();
-
+        // handling pass code submition
         $("#enter_code_form").submit(function (e) {
             e.preventDefault();
-            // disabling inputs
-            $("input").each(function () {
-                $(this).attr("disabled", "disabled");
-            });
-
-            $("#codeInput").addClass("loading");
+            disableUI();
             // making data
             var code = $('#codeInput').prop('value');
             var hash = Crypto.SHA256(code);
             // sending login info
             $.post('Home/EnterCode', { code: hash }, function (response) {
-                
-                //revert ui disabling
-                $("input").each(function () { $(this).removeAttr("disabled"); });
-                $("#codeInput").removeClass("loading");
-
+                enableUI();
+                // handling response
                 if (!response.error) {
-                    window.location = '/operations';
+                    // redirect to home
+                    window.location = '/';
                 } else {
                     ui.handleError(response.error);
                 }
             }).error(function () {
-                ui.showError('For some strange(probably, magic?) reason, ' 
+                ui.showError('For some strange(probably, magic?) reason, '
                     + 'ajax reqest could not reach the server. Please, inform system administrator.');
-                
-                //revert ui disabling
-                $("input").each(function () { $(this).removeAttr("disabled"); });
-                $("#codeInput").removeClass("loading");
-            })         
+                enableUI();
+            })
         });
     });
+
+    function disableUI() {
+        $("input").each(function () {
+            $(this).attr("disabled", "disabled");
+            $("#codeInput").addClass("loading");
+        });
+    }
+
+    function enableUI() {
+        $("input").each(function () { $(this).removeAttr("disabled"); });
+        $("#codeInput").removeClass("loading");
+        $('#codeInput').focus();
+    }
 })();
